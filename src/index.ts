3c87b519da99bd53871b182d4d944c2d0dbc108f
CozyCrypto <fassdavid722@gmail.com>
2026-07-11 01:19:56 +0100

    feat: scaffold learning engine - src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { getGroqClient } from "./groq-client.js";
+export { ResearchEngine, ResearchResult, ResearchResultSchema } from "./Research/research.js";
+export { ReflectionEngine, ReflectionResult, ReflectionResultSchema } from "./Reflection/reflection.js";
+export { VerificationEngine, VerificationResult, VerificationResultSchema } from "./Verification/verification.js";
+export { KnowledgeBuilder, KnowledgeEntry, KnowledgeEntrySchema } from "./KnowledgeBuilder/knowledgebuilder.js";
+export { ImprovementTracker, OutcomeRecord, ImprovementMetrics } from "./Improvement/improvement.js";
